36466938c5ac5cc613cc0412fe9a308ba8196855
Nicholas Maestrello Agiz <rainickmr@gmail.com>
2018-05-16 10:39:04 -0300

    correção rota

--- src/app/router/app-routing/app-routing.module.ts
+++ src/app/router/app-routing/app-routing.module.ts
@@ -2,20 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-import {LoginComponent} from '../../components/login/login.component'
-import {DashboardComponent} from '../../components/dashboard/dashboard.component'
-import {HomeComponent} from '../../components/home/home.component'
+import { LoginComponent } from '../../components/login/login.component'
+import { DashboardComponent } from '../../components/dashboard/dashboard.component'
+import { HomeComponent } from '../../components/home/home.component'
+import { AlunoComponent } from '../../components/aluno/aluno/aluno.component';
+import { NotFoundComponent } from '../../shared/component/not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'home', component: HomeComponent},
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', redirectTo: '/home', pathMatch: 'full'}
+  {
+    path: 'dashboard', component: DashboardComponent, children: 
+    [
+      { path: 'home', component: HomeComponent },
+      { path: 'aluno', component: AlunoComponent }
+    ]
+  },
+  { path: 'notfound', component: NotFoundComponent },
+  { path: '', redirectTo: '/notfound', pathMatch: 'full' },
+  { path: '**', redirectTo: '/notfound', pathMatch: 'full' }
 ]
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 
